10b37fae6038fe8fdb940d7ce9fe9a7fb9586679
Philippe Pagé <116277312+philippe-page@users.noreply.github.com>
2024-07-17 11:35:02 -0400

    updated example calculator agent chat

--- calculator_agent.py
+++ calculator_agent.py
@@ -1,31 +1,43 @@
 from taskflowai.task import Task
+from taskflowai.agent import Agent
 from taskflowai.tools import CalculatorTools
 from taskflowai.llm import OpenrouterModels
-from taskflowai.agent import Agent
+from taskflowai.utils import Utils
 
 math_tutor = Agent(
     role="math tutor",
     goal="answer user queries accurately, with expanded explanations",
-    attributes="patient, encouraging, and knowledgeable about various areas of mathematics",
+    attributes="patient, encouraging, and knowledgeable about various areas of mathematics. you use a calculator to help your students when necessary",
     llm=OpenrouterModels.haiku
 )
 
-def respond_to_query(math_tutor, user_query):
+def respond_to_query(math_tutor, user_query, history):
     response = Task.create(
         agent=math_tutor,
-        instruction="Answer the user query in a conversational way",
-        context=f"User query: '{user_query}'",
+        instruction=f"Answer the user query in a conversational way. User query to respond to: '{user_query}'",
+        context=f"Conversation History:\n{history}\n\nUser query: '{user_query}'",
         tools={
             "Calculator": CalculatorTools.basic_math,
-            "Get Current Time": CalculatorTools.get_current_time,
+            "Get Current Time": CalculatorTools.get_current_time
         }
     )
     return response
 
+
 def main():
-    user_query = input("Enter a user query: ")
-    response = respond_to_query(math_tutor, user_query)
-    print(f"Final response: {response}")
+    history = []
+    while True:
+        user_query = input("\nEnter a question (or 'exit' to quit):\n")
+        if user_query.lower() == 'exit':
+            break
+
+        formatted_usermessage = Utils.format_message("User", user_query)
+        Utils.update_history(history, formatted_usermessage)
+        response = respond_to_query(math_tutor, user_query, history)
+        formatted_response = Utils.format_message("Assistant", response)
+        Utils.update_history(history, formatted_response)
+        print(f"Assistant: {response}")
+
 
 if __name__ == "__main__":
     main()
